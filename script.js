b79f44411b554d497062b657d58b3c8e31deb83e
JORF4R <123892522+JORF4R@users.noreply.github.com>
2025-03-08 22:04:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let model;
 
 // Cargar el modelo
 async function loadModel() {
-    model = await tf.loadLayersModel('modelo_tfjs_model/model.json');
+    model = await tf.loadLayersModel('https://raw.githubusercontent.com/JORF4R/iandoando/refs/heads/main/modelo_tfjs_model/model.json');
     console.log("Modelo cargado correctamente.");
 }
 
